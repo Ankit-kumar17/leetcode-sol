e516cfed6f52a561f98941559edeeee2f6b222fd
Ankit Kumar <ankits.kumar.cg@gmail.com>
2026-07-21 16:35:48 +0530

    Create 66-PlusOne

---
+++ https:/github.com/Ankit-kumar17/leetcode-sol.git/66-PlusOne/66-PlusOne.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/21/2026, 4:35:48 PM
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        // agar sab 9 the
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
