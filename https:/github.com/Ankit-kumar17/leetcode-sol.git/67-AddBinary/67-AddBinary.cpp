2d260e1910ca36dd3deb949c882a9b55a41548d2
Ankit Kumar <ankits.kumar.cg@gmail.com>
2026-07-21 16:35:46 +0530

    Create 67-AddBinary

---
+++ https:/github.com/Ankit-kumar17/leetcode-sol.git/67-AddBinary/67-AddBinary.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/21/2026, 4:35:46 PM
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result = "";
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+
+            result += (sum % 2) + '0';
+            carry = sum / 2;
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
