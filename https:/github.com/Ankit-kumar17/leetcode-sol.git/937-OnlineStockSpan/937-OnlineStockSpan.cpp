75368bc351eac72ea6b2173cbb4e8c52bf7f73fb
Ankit Kumar <ankits.kumar.cg@gmail.com>
2026-07-21 16:34:56 +0530

    Create 937-OnlineStockSpan

---
+++ https:/github.com/Ankit-kumar17/leetcode-sol.git/937-OnlineStockSpan/937-OnlineStockSpan.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/21/2026, 4:34:55 PM
+class StockSpanner {
+public:
+    stack<pair<int , int>> st;
+    
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int span = 1;
+        while(!st.empty() && st.top().first <= price){
+
+            span += st.top().second;
+            st.pop();
+
+        }
+        st.push({price , span});
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
