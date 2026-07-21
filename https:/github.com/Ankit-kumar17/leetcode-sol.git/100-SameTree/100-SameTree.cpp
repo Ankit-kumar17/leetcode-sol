9f4e4fd61bd0aea76679658134b1c031eadb5fdd
Ankit Kumar <ankits.kumar.cg@gmail.com>
2026-07-21 16:35:39 +0530

    Create 100-SameTree

---
+++ https:/github.com/Ankit-kumar17/leetcode-sol.git/100-SameTree/100-SameTree.cpp
@@ -0,0 +1,23 @@
+// Last updated: 7/21/2026, 4:35:38 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL) return true;
+
+        if (p == NULL || q == NULL) return false;
+
+        return (p->val == q->val) &&
+               isSameTree(p->left, q->left) &&
+               isSameTree(p->right, q->right);
+    }
+};
